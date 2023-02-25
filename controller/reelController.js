5eb81d1023829a065f2ce53e9d6fc84a0e35b5f8
alwaysalazycoder <gs314315316@gmail.com>
2023-02-25 17:58:41 +0530

    reel uploading setup

--- controller/reelController.js
+++ controller/reelController.js
@@ -1,13 +1,35 @@
 
 const Reel = require("../models/reelModel");
+import cloudinary from "cloudinary";
+
 exports.postReel = async(req,res,next)=>{
 
-    const {public_id,public_url,reel_title} = req.body;
+    const {reel_title} = req.body;
 
+    let myCloud;
+
+    try{
+        myCloud = await cloudinary.v2.uploader.upload(req.body.reelVideo,{
+            chunk_size: 6000000,
+            folder : "Designwale-reels",
+            resource_type:"auto",
+
+        })
+    }
+    catch(error){
+        console.log("Error in uploading reels:: ",Erro);
+    }
+
+    if(!myCloud){
+        res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
     const reel = await Reel.create({
         reel_title,
-        public_id,
-        public_url
+        public_id:myCloud.public_id,
+        public_url:myCloud.secure_url
     });
 
     if(!reel){
